a4373338ff68618791a73c31450f80c0117ea2bf
Azizur Rahman(Laptop) <aremon2533@gmail.com>
2023-11-03 03:26:31 +0600

    final

--- src/app/component/GallerySection.tsx
+++ src/app/component/GallerySection.tsx
@@ -1,10 +1,9 @@
 "use client";
 import React, { useState } from "react";
 import { imageList } from "../data/PlaceHolderData";
-import GridItem from "./GridItem";
-import AddImage from "./AddImage";
-import styles from "./component.module.css";
+import styles from "./GallerySection.module.css";
 import { ReactSortable } from "react-sortablejs";
+
 interface ItemProps {
   id: number;
   imgSrc: string;
@@ -12,28 +11,113 @@ interface ItemProps {
 
 const GallerySection = () => {
   const [state, setState] = useState<ItemProps[]>(imageList);
+  const [itemSelected, setItemSelected] = useState<number[]>([]);
+  const [clearSelection, setClearSelection] = useState(false); // Added clearSelection state
+
   const addImage = (imgSrc: string) => {
     const newItem = {
-      id: state.length, // Assign a unique ID to the new item
+      id: state.length,
       imgSrc: imgSrc,
     };
-
     setState((prevItems) => [...prevItems, newItem]);
   };
+
+  const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        if (e.target) {
+          addImage(e.target.result! as string);
+        }
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const toggleSelection = (id: number) => {
+    if (itemSelected.includes(id)) {
+      setItemSelected(itemSelected.filter((itemId) => itemId !== id));
+    } else {
+      setItemSelected([...itemSelected, id]);
+    }
+  };
+
+  const deleteSelectedItems = () => {
+    const filteredState = state.filter(
+      (item) => !itemSelected.includes(item.id)
+    );
+    setState(filteredState);
+    setItemSelected([]);
+  };
+
+  const handleClearSelection = () => {
+    setItemSelected([]);
+    setClearSelection(!clearSelection);
+  };
+
   return (
     <>
       <div className={styles.header_container}>
-        <h2>Gallery</h2>
+        {itemSelected.length === 0 ? (
+          <h2>Gallery</h2>
+        ) : (
+          <div className={styles.selected_header}>
+            <div className={styles.selected_count}>
+              <input
+                type="checkbox"
+                id="clear-selection"
+                checked={clearSelection}
+                onChange={handleClearSelection}
+              />
+              <p>{`${itemSelected.length} ${
+                itemSelected.length === 1 ? "File" : "Files"
+              } Selected`}</p>
+            </div>
+            <button
+              className={styles.delete_btn}
+              type="button"
+              onClick={deleteSelectedItems}
+            >
+              {itemSelected.length === 1 ? "Delete File" : "Delete Files"}
+            </button>
+          </div>
+        )}
       </div>
+
       <ReactSortable
         className={styles.grid_container}
         list={state}
         setList={setState}
       >
         {state.map((item) => (
-          <GridItem key={item.id} item={item} />
+          <div className={styles.grid_item} key={item.id}>
+            <div className={styles.single_img_container}>
+              <img src={item.imgSrc} alt={`gallery-img-${item.id}`} />
+              <div
+                className={`${styles.grid_input_container} ${
+                  itemSelected.includes(item.id) ? styles.selected_item : ""
+                }`}
+                onClick={() => toggleSelection(item.id)}
+              >
+                <input
+                  type="checkbox"
+                  name="checkbox"
+                  id={item.id.toString()}
+                  checked={itemSelected.includes(item.id)}
+                />
+              </div>
+            </div>
+          </div>
         ))}
-        <AddImage addImage={addImage} />
+        <label htmlFor="file-input" className={styles.add_image_container}>
+          <img src="/images/add-photo-icon.png" alt="icon" />
+          <h3>Add Photo</h3>
+          <input type="file" id="file-input" onChange={handleImageUpload} />
+        </label>
       </ReactSortable>
     </>
   );
